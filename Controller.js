3f43e89e0b6c729d270945d6b638afeea42ce766
jjwill <jjwill@hotmail.co.uk>
2016-07-15 12:13:34 +0100

    Files deleted and working angularjs array added

--- Controller.js
+++
@@ -1,6 +0,0 @@
-var myApp = angular.module('Controller', []);
-console.log("Controller_1");
-myApp.controller('ContactController', ['$scope', function($scope) {
-    console.log("Controller_2");
-	$scope.contacts = ["1 - A", "2 - B", "3 - C", "4 - D", "5 - E", "6 - E", "7 - E", "8 - E", "9 - F", "10 - G"];
-}]);
---
+++ ctrl.js
@@ -0,0 +1,4 @@
+var app = angular.module("myApp", []);
+app.controller("myCtrl", function($scope) {
+    $scope.records = ["1 - A", "2 - B", "3 - C", "4 - D", "5 - E", "6 - E", "7 - E", "8 - E", "9 - F", "10 - G"]
+});
