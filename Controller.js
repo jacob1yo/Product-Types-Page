e7a391a47a8d331af46d41af765265ba11d631ca
jjwill <jjwill@hotmail.co.uk>
2016-07-15 17:45:47 +0100

    Structure of code now neater

--- Controller.js
+++ Controller.js
@@ -1,4 +1,13 @@
 var app = angular.module("myApp", []);
 app.controller("myCtrl", function($scope) {
-    $scope.records = ["1 - Product A", "2 - Product B", "3 - Product C", "4 - Product D", "5 - Product E", "6 - Product E", "7 - Product E", "8 - Product E", "9 - Product F", "10 - Product G"]
+    $scope.records = ["1 - Product A", 
+                      "2 - Product B", 
+                      "3 - Product C", 
+                      "4 - Product D", 
+                      "5 - Product E", 
+                      "6 - Product E", 
+                      "7 - Product E", 
+                      "8 - Product E", 
+                      "9 - Product F", 
+                      "10 - Product G"]
 });
